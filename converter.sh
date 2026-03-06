0a22bc3743499f753215ac49283d674b1d987cff
Zsolt <fedor.zsolt@lego.com>
2026-03-06 09:52:58 +0100

    modified converter.sh wont ignore .mov files anymore, can be ran from anywhere - asks for target folder first

--- converter.sh
+++ converter.sh
@@ -1,7 +1,18 @@
+read -p "Enter the target folder path: " target_folder
+
+if [ ! -d "$target_folder" ]; then
+    echo "Directory does not exist. Please check the path and try again."
+    exit 1
+fi
+
+cd "$target_folder"  && \
 mkdir -p converted && \
-for f in *.MOV; do
-    ffmpeg -i "$f" -vf scale=1920:1080 -c:v libx264 -c:a aac "${f%.MOV}.mp4" && \
+for f in *.MOV *.mov; do
+    [ -f "$f" ] || continue
+    outname="${f%.MOV}"
+    outname="${outname%.mov}"
+    ffmpeg -i "$f" -vf scale=1920:1080 -c:v libx264 -c:a aac "${outname}.mp4" && \
     echo "file converted: $f"
 done && \
 mv *.mp4 converted/ && \
-echo "Files converted and moved to the directory."
+echo "Files have been converted ! Output files can be found in $target_folder/converted"
